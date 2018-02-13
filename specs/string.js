876db076ff3a3866891032897a5158ad1f8e604a
ciamiz <luca@ramundo.info>
2018-02-13 19:03:08 +0100

    Refine specs for changed expectations

--- specs/string.js
+++ specs/string.js
@@ -8,7 +8,7 @@ describe("string", () => {
     "Programming Puzzles & Code Golf": "programmingPuzzlesCodeGolf",
     "XML HTTP request": "xmlHttpRequest",
     "supports IPv6 on iOS?": "supportsIpv6OnIos",
-    "SomeThing w1th, apo'strophe's and' punc]tuation": "someThingW1thApostrophesAndPuncTuation",
+    "SomeThing w1th, apo'strophe's and' punc]tuation": "someThingW1thApostrophesAndPunctuation",
     "nothing special": "nothingSpecial",
     "5pecial ca5e": "5pecialCa5e",
     "1337": "1337",
@@ -16,7 +16,12 @@ describe("string", () => {
     "whatA mess": "whataMess",
     "abcD": "abcd",
     "a": "a",
-    "B": "b"
+    "B": "b",
+    "snake_case": "snakeCase",
+    "long_snake_case": "longSnakeCase",
+    "dash-case": "dashCase",
+    "long-dash-case": "longDashCase",
+    "SomeThing w1th": "someThingW1th"
   }
 
   Object.keys(matches).forEach(test => {
