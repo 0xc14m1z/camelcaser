4a3b594def3d9a3bc396eef78f9c313189133dcf
Luca Ramundo <luca@ramundo.info>
2018-02-17 01:34:53 +0100

    Refine objectKeys method and add specs

--- specs/objectKeys.js
+++ specs/objectKeys.js
@@ -11,10 +11,24 @@ describe("objectKeys", () => {
     expect(objectKeys(test)).to.be.deep.equal(expected)
   })
 
+  it('should keep a literal value the same', () => {
+    const test = 42
+    const expected = 42
+
+    expect(objectKeys(test)).to.be.deep.equal(expected)
+  })
+
+  it('should keep an empty array the same', () => {
+    const test = []
+    const expected = []
+
+    expect(objectKeys(test)).to.be.deep.equal(expected)
+  })
+
   it('should camelCase keys in a flat object', () => {
     const test = {
-      first_key: 12, 
-      SecondKey: 23, 
+      first_key: 12,
+      SecondKey: 23,
       "third-key": 34
     }
 
@@ -29,8 +43,8 @@ describe("objectKeys", () => {
 
   it('should camelCase keys in an object with nested objects', () => {
     const test = {
-      first_key: 12, 
-      SecondKey: 23, 
+      first_key: 12,
+      SecondKey: 23,
       "third-key": 34,
       "_fourth_key": {
         first_nestedKey: 45,
@@ -53,6 +67,51 @@ describe("objectKeys", () => {
         }
       }
     }
+
+    expect(objectKeys(test)).to.be.deep.equal(expected)
+  })
+
+  it('should camelCase keys in an object inside an array', () => {
+    const test = {
+      first_key: 12,
+      SecondKey: 23,
+      "third-key": 34,
+      "_fourth_key": {
+        first_nestedKey: 45,
+        SecondNestedKey: 56,
+        "third_nested-key": {
+          first_nested_nested_key: 67,
+          second_nested_nested_key: [
+            { first_nested_nestedNestedKey: 78 },
+            { second_nested_nestedNestedKey: 87 }
+          ]
+        }
+      }
+    }
+
+    const expected = {
+      firstKey: 12,
+      secondKey: 23,
+      thirdKey: 34,
+      fourthKey: {
+        firstNestedKey: 45,
+        secondNestedKey: 56,
+        thirdNestedKey: {
+          firstNestedNestedKey: 67,
+          secondNestedNestedKey: [
+            { firstNestedNestedNestedKey: 78 },
+            { secondNestedNestedNestedKey: 87 }
+          ]
+        }
+      }
+    }
+
+    expect(objectKeys(test)).to.be.deep.equal(expected)
+  })
+
+  it('should camelCase the keys of objects contained into an array', () => {
+    const test = [ { nested_key: 42 } ]
+    const expected = [ { nestedKey: 42 } ]
 
     expect(objectKeys(test)).to.be.deep.equal(expected)
   })
--- src/objectKeys.js
+++ src/objectKeys.js
@@ -1,25 +1,24 @@
 import CamelCaser from '..'
 
-const objectKeys = o =>
-  Object
-    .keys(o)
-    .reduce(
-      (result, key) => 
-        Object.assign(
-          {}, 
-          result, 
-          { 
-            [CamelCaser.string(key)]: isObject(o[key]) ? objectKeys(o[key]) : o[key]
-          }
-        ),
-      {}
-    )
+const objectKeys = obj =>
+  isObject(obj) ?
+  Object.keys(obj).reduce(objectKey(obj), {}) :
+    Array.isArray(obj) ?
+    objectsArrayKeys(obj) :
+    obj
 
 export default objectKeys
 
-const isObject = o =>
-  o !== null && 
-  (
-    typeof obj === "object" || 
-    Object.getPrototypeOf(o) == Object.prototype
+const objectKey = initialObject => (obj, key) =>
+  Object.assign(
+    obj,
+    { [ CamelCaser.string(key) ]: objectKeys(initialObject[key]) }
   )
+
+const objectsArrayKeys = arr =>
+  arr.map(element => objectKeys(element))
+
+const isObject = obj =>
+  obj !== null &&
+  typeof obj === "object" &&
+  Object.getPrototypeOf(obj) === Object.prototype
